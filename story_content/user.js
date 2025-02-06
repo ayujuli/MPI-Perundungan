a91a535ba2bdd0ce835f7586c1bb844a0affaf1e
ayujuli <akuntugasayu@gmail.com>
2025-02-06 13:24:35 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,22 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "65BVYkz5zIe":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+
+}
+
